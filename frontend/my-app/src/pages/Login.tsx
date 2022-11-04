a03b880734aae374324a9c0e0cbabf2fe64adc7c
user <user@Laptop.localdomain>
2022-11-04 23:27:14 +0100

    login [age

--- frontend/my-app/src/pages/Login.tsx
+++ frontend/my-app/src/pages/Login.tsx
@@ -1,11 +1,14 @@
 import Button_42_Connect from "../componenets/Button_42_connect"
 import NavBar from "../componenets/NavBar"
+import "../styles/Login.css"
 
 const Login = () => {
 	return (
 		<div>
 			<NavBar />
-			<Button_42_Connect />
+			<div className="btn-center-lgn">
+				<Button_42_Connect />
+			</div>
 		</div>
 	)
 }
