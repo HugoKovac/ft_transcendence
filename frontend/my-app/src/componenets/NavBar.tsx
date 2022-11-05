9c01ce97f3ed75b30cd9777c068678b03c6ee650
user <user@Laptop.localdomain>
2022-11-05 14:56:51 +0100

    new structue of NavBar

--- frontend/my-app/src/componenets/NavBar.tsx
+++ frontend/my-app/src/componenets/NavBar.tsx
@@ -1,20 +1,55 @@
 import { NavLink } from "react-router-dom"
 import "../styles/NavBar.css"
 
+type props_right_bar = { logged_in: boolean }
+
+function Login_button(){
+	return (
+		<NavLink to='/login' className={(nav) : any => (nav.isActive ? "ancr nav-active" : "ancr")}>
+				<li>Login</li>
+		</NavLink>
+	)
+}
+
+function Profile_button(){
+	return (
+		<NavLink to='/profile' className={(nav) : any => (nav.isActive ? "ancr nav-active" : "ancr")}>
+				<li>Profile</li>
+		</NavLink>
+	)
+}
+
+function Left_bar(){
+	return (
+		<ul className="left-NavBar">
+			 <NavLink to='/' className={(nav) : any => (nav.isActive ? "ancr nav-active" : "ancr")}>
+				<li>Home</li>
+			</NavLink>
+		</ul>
+	)
+}
+
+function Right_bar(props : props_right_bar){
+
+	let button;
+
+	if (props.logged_in)
+		button = <Profile_button />
+	else
+		button = <Login_button />
+
+	return (
+		<ul className="right-NavBar">
+			{button}
+		</ul>
+	)
+}
+
 const NavBar = () => {
 	return (
 		<div className="NavBar">
-			<ul className="left-NavBar">
-			 <NavLink to='/' className="ancr">
-				<li>Home</li>
-			</NavLink>
-			</ul>
-			<ul className="right-NavBar">
-			<NavLink to='/login' className="ancr">
-				{/*! if logged account */}
-				<li>login</li>
-			</NavLink>
-			</ul>
+			<Left_bar />
+			<Right_bar logged_in={false} />
 		</div> 
 	)
 }
