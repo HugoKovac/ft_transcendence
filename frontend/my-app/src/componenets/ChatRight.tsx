d19dcd3d4863320d01c9020c406cf61dde3ce4e6
user <user@Laptop>
2022-12-01 08:52:26 +0100

    fix unused var

--- frontend/my-app/src/componenets/ChatRight.tsx
+++ frontend/my-app/src/componenets/ChatRight.tsx
@@ -1,6 +1,4 @@
 import ChatInput from "./ChatInput"
-import ChooseFriend from "./ChooseFriend"
-import Popup from "./Popup"
 
 const ChatRight = (props : {conv:number, msgList : JSX.Element[], setNewMsg: (v:boolean)=>void}) => {
 	return <div className='chatBox'>
